e19fc6d46e9baf3698fc7b4c942e117f54d2bbb8
Warai777 <rodneysanchez1234@gmail.com>
2026-04-16 00:30:56 -0400

    Update build.js

--- build.js
+++ build.js
@@ -3,4 +3,6 @@ let html = fs.readFileSync('index.html', 'utf8');
 html = html.replace('__ANTHROPIC_KEY__', process.env.ANTHROPIC_API_KEY || '');
 html = html.replace('__SUPABASE_URL__', process.env.SUPABASE_URL || '');
 html = html.replace('__SUPABASE_KEY__', process.env.SUPABASE_KEY || '');
+fs.mkdirSync('dist', {recursive:true});
 fs.writeFileSync('dist/index.html', html);
+console.log('Built successfully');
