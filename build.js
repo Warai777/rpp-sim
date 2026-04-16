789f1dfafedf25a18948be75d1d58bb3c1d9a657
Warai777 <rodneysanchez1234@gmail.com>
2026-04-16 00:18:32 -0400

    Create build.js

---
+++ build.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+let html = fs.readFileSync('index.html', 'utf8');
+html = html.replace('__ANTHROPIC_KEY__', process.env.ANTHROPIC_API_KEY || '');
+html = html.replace('__SUPABASE_URL__', process.env.SUPABASE_URL || '');
+html = html.replace('__SUPABASE_KEY__', process.env.SUPABASE_KEY || '');
+fs.writeFileSync('dist/index.html', html);
